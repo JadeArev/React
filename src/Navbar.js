45b4bc04b028590912b91a52186efda4098f60ae
Jade <jade.arevort@gmail.com>
2022-06-08 09:02:26 -0300

    React

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+export default Navbar = () => {
+
+    return (
+    
+        <ul>
+            <li>Home</li>
+            <li>Quienes somos</li>
+            <li>Info</li>
+        </ul>
+    );
+}
